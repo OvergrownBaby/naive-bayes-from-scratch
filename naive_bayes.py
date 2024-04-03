190fbcc1229bbab5a4a692caee409be10dea24bc
OvergrownBaby <andytsangyuklun@gmail.com>
2024-04-03 17:25:16 +0800

    Added log likelihood

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,36 +1,60 @@
 import pandas as pd
 import numpy as np
 
-class bernoulliNB():
-    def __init__(self):
-        self.model = None
+class BernoulliNB():
+    def __init__(self, log_likelihood=False):
+        self.model = {}
+        self.log_likelihood = log_likelihood
+        print("Model Parameters:")
+        print("Log Likelihood: ", self.log_likelihood)
 
     def fit(self, X: pd.DataFrame, y: pd.Series):
         labels = y.unique()
         features = X.columns
 
-        ret = {}
-        for label in labels:
-            p_label = (y == label).mean()
-            dataset = X[y == label]
-            likelihoods = {}
-            for feature in features:
-                likelihoods[feature] = (dataset[feature] == 1).mean()
-            ret[label] = (p_label, likelihoods)
+        ### uses log likelihood to avoid underflow + laplace smoothing
+        if self.log_likelihood:
+            # Compute the log prior probability for each class
+            log_prior = np.log(y.value_counts(normalize=True))
+            
+            # Compute the log likelihood for each feature given each class
+            
+            for label in labels:
+                subset = X[y == label]
+                log_likelihood = {}
+                # Use Laplace smoothing to avoid log(0)
+                for feature in features:
+                    feature_count = subset[feature].sum()
+                    log_likelihood[feature] = np.log((feature_count + 1) / (len(subset) + 2))
+                self.model[label] = (log_prior[label], log_likelihood)
 
-        self.model = ret
+        ### base implementation
+        else:
+            for label in labels:
+                p_label = (y == label).mean()
+                dataset = X[y == label]
+                likelihoods = {}
+                for feature in features:
+                    likelihoods[feature] = (dataset[feature] == 1).mean()
+                self.model[label] = (p_label, likelihoods)
+
 
     def predict(self, X: pd.DataFrame):
         def get_posterior(label, feature_vec):
-            # print([feature for feature in feature_vec.index])
-            # print([feature for feature in feature_vec.columns()])
-            return self.model[label][0] * np.prod([self.model[label][1][feature] if feature_vec[feature] == 1 else 1 - self.model[label][1][feature] for feature in feature_vec.index])
+            if self.log_likelihood:
+                return self.model[label][0] + np.sum(
+                    [self.model[label][1][feature] if feature_vec[feature] == 1 
+                    else np.log1p(-np.exp(self.model[label][1][feature])) 
+                    for feature in feature_vec.index])
+            else:
+                return self.model[label][0] * np.prod(
+                    [self.model[label][1][feature] if feature_vec[feature] == 1 
+                    else 1 - self.model[label][1][feature] 
+                    for feature in feature_vec.index])
         
         y_pred = []
-        for idx, row in X.iterrows():
-            get_posterior(1, row.drop('text'))
-            y_pred.append(max(self.model.keys(), key=lambda label: get_posterior(label, row.drop('text'))))
-        # return max(self.model.keys(), key=lambda label: get_posterior(label, X.columns.drop('text')))
+        for _, row in X.iterrows():
+            y_pred.append(max(self.model.keys(), key=lambda label: get_posterior(label, row)))
         return y_pred
 
     def score(self, X, y):
