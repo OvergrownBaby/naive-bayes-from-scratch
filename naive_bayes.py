04858358807553a914db092db746a7038f88cd8c
OvergrownBaby <andytsangyuklun@gmail.com>
2024-04-09 17:09:40 +0800

    Modified MultinomialNB() to accept sparse matrix as training set. Added Tfidf to example_mode.ipynb

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,51 +1,55 @@
 import pandas as pd
 import numpy as np
 from abc import ABC
+import numpy as np
+from scipy import sparse
 
 class MultinomialNB():
     def __init__(self, log=False, alpha=1):
         self.log = log
         self.alpha = alpha
+        self.model = {}  # {label: (class_prob, feature_probs)}
 
-        self.model = {} # {label: (class_prob, feature_counts, feature_probs)}
+    def fit(self, X, y):
+        labels = np.unique(y)
+        class_probs = {label: (y == label).mean() for label in labels}
 
-    def fit(self, X: pd.DataFrame, y: pd.Series):
-        labels = y.unique()
-        class_probs = y.value_counts(normalize=True)
-
-        num_of_keywords = len(X.columns)
-        alpha = self.alpha
+        num_of_features = X.shape[1]
         
         for label in labels:
             subset = X[y == label]
-            total_count = sum(subset.sum())
-            feature_probs = (subset.sum() + alpha) / (total_count + alpha*num_of_keywords)
-            self.model[label] = (class_probs[label], feature_probs.to_dict())
+            total_count = subset.sum(axis=0)
+            feature_probs = (total_count + self.alpha) / (total_count.sum() + self.alpha * num_of_features)
+            if isinstance(feature_probs, np.matrix):
+                feature_probs = feature_probs.tolist()[0]
+            self.model[label] = (class_probs[label], feature_probs)
+        return self.model
 
-    def get_posterior(self, label, feature_vec: pd.DataFrame):
-        """
-        argmax p(C|X) = p(C)p(X|C)\n
-        where p(X|C) = m_coeff * p(x_1|C)^x_1 * ... * p(x_n|C)^x_n\n
-        but after log transform and optimization, \n
-        p(X|C) = p(x_1|C)^x_1 + ... + p(x_n|C)^x_n
-        where p(x_i|C) = (count of word i in class + a)/(total words in class)
-        """
+    def get_posterior(self, label, feature_vec):
         class_prob = self.model[label][0]
         feature_probs = self.model[label][1]
-        
-        # log(p(class)) + log(p(x_1|class)) + ... + log(p(x_n|class))
-        return np.log(class_prob) + np.sum([
-            np.log(feature_probs[feature]) if feature_vec[feature] == 1 
-            else np.log1p(-feature_probs[feature]) 
-            for feature in feature_vec.index
-        ])
 
-    def predict(self, X: pd.DataFrame):
+        # Convert sparse feature vector to a dense format
+        if isinstance(feature_vec, sparse._csr.csr_matrix):
+            feature_vec = feature_vec.toarray()[0]
+        
+        log_prob = np.log(class_prob)
+        for i in range(len(feature_vec)):
+            feature_prob = feature_probs[i] if i < len(feature_probs) else self.alpha
+            log_prob += feature_vec[i] * np.log(feature_prob)
+        
+        return log_prob
+
+
+    def predict(self, X):
         y_pred = []
-        for _, row in X.iterrows():
+        for i in range(X.shape[0]):
+            if isinstance(X, pd.DataFrame):
+                row = X.iloc[i]
+            elif isinstance(X, sparse._csr.csr_matrix):
+                row = X.getrow(i)
             y_pred.append(max(self.model.keys(), key=lambda label: self.get_posterior(label, row)))
-        return y_pred
-    
+        return np.array(y_pred)
 
 # todo: add alpha parameter for laplace smoothing
 class BernoulliNB():
